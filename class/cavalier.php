8ccaa9656824793bbf10f14f1fefecd7c73e44c1
Fendrir <papao1@hotmail.fr>
2017-11-16 16:28:24 +0100

    initialisation du projet soldat

---
+++ class/cavalier.php
@@ -0,0 +1,14 @@
+<?php
+
+class Cavalier extends Armee{
+
+    private $cheval;
+
+    public function __construct($_arme,$_nbHomme){
+
+        $this->_arme="Fusil automatique";
+        $this->_nbHomme=30;
+
+    }
+
+}
---
+++ class/soldat.php
@@ -0,0 +1,13 @@
+<?php
+
+class Soldat extends Armee{
+
+    public function __construct($_arme,$_nbHomme){
+
+        $this->_arme="fusil baïonnettes";
+        $this->_nbHomme=70;
+
+    }
+
+
+}
